37e36e6ed7f93be5e741dd3c5a35b3dacc529c45
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2024-08-09 08:50:31 -0300

    feat: implementation shared ios location

--- iosAppYourInterests/iosAppYourInterests/Ui/FavoriteScreen/NearbyInterestsState.swift
+++ iosAppYourInterests/iosAppYourInterests/Ui/FavoriteScreen/NearbyInterestsState.swift
@@ -10,44 +10,10 @@ import Foundation
 import MapKit
 import shared
 
-
 @MainActor
 class NearbyInterestsState: ObservableObject {
 	
-	@Published var loadingState: LoadingState = .loading
-  @Published	var location: CLLocationCoordinate2D? = nil
-  @Published var viewModel = RecoveryLocation()
-  var gelocationError: GeolocationError? = nil
-	
-	
-
-	
-
-	func fetchLocation() async {
-		self.loadingState = .loading
-		viewModel.getLocation()
-		for await viewModel in viewModel.location {
-			
-			if(viewModel.exception != nil) {
- 
-				if let exception = viewModel.exception as? GeolocationException {
-					self.loadingState = .failure
-					self.gelocationError = exception.error
-					print(exception.messageError ?? "")
-				}
-			
-			}
-			
-			if(viewModel.data != nil) {
-				self.loadingState = .success
-				self.location = CLLocationCoordinate2D(latitude: viewModel.data!.latitude, longitude: viewModel.data!.longitude)
-				
-			}
-			
-		}
-		
-		
-	}
+	 
 	
 	
 	
